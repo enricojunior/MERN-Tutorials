8713c6f701cd395e037543f629112584f104e115
enricojunior <zethsuboaragonchemistry@gmail.com>
2024-06-08 22:14:17 +0800

    attach workouts route

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // Express app
 const app = express()
 
-// Route handler
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the application..'})
+// Acts as a middleware
+app.use(express.json()) 
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+// Route handler
+app.use('/api/workouts' ,workoutRoutes)
 
 // Listening to a certain port number (Listening requests)
 app.listen(process.env.PORT, () => {
-    console.log('Listening to port 4000!')
+    console.log('Listening to port 4000!', process.env.PORT)
 })
