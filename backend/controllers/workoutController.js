0af84af64dd7f9a48da038a5339da1a6948d6231
enricojunior <zethsuboaragonchemistry@gmail.com>
2024-06-09 19:51:15 +0800

    Add Controllers

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,49 @@
+const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
+
+// Get All Workouts
+const getWorkouts = async (req, res) => {
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+}
+
+// Get A Single Workout
+const getWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findById(id)
+    if(!workout){
+        return res.status(404).json({error: 'Unable to find the specified Workout!'})
+    }
+    res.status(200).json(workout)
+}
+
+// Create A New Workout
+const createWorkout = async (req, res) => {
+    const {title, load, reps} = req.body
+
+    // Add Document to DB
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+// Delete A Workout
+
+
+// Update A Workout
+
+
+// Export the modules
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout
+}
