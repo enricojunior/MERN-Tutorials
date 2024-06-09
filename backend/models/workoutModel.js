863ebff28e282a4a970497e9270481c0e66ea9b1
enricojunior <zethsuboaragonchemistry@gmail.com>
2024-06-09 19:15:16 +0800

    Attach Models & Schemas to the backend folder

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,23 @@
+// Mongoose is used to create models and schemas
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+// Create a new workout schema
+const workoutSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    load: {
+        type: Number,
+        required: true
+    }
+}, {timestamps: true}) // When we try to create a new document, it automatically adds a created app property for us to 
+                       // say when the document was created which is nice
+
+module.exports = mongoose.model('Workout', workoutSchema)
