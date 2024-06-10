efa0586585b7d7d71d98467803f12786dd6675f0
enricojunior <zethsuboaragonchemistry@gmail.com>
2024-06-10 09:21:00 +0800

    Insert deleteWorkout, createWorkout method

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -36,14 +36,44 @@ const createWorkout = async (req, res) => {
 }
 
 // Delete A Workout
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findOneAndDelete({_id: id})
+    if(!workout){
+        return res.status(400).json({error: 'Unable to find the specified Workout!'})
+    }
+
+    res.status(200).json(workout)
+}
 
 // Update A Workout
+const updateWorkout = async (req, res) => {
+    const { id } = req.params
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if(!workout){
+        return res.status(400).json({error: 'Unable to find the specified Workout!'})
+    }
+    res.status(200).json(workout)
+}
 
 // Export the modules
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 }
