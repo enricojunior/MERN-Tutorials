b5a09fbf49777f5ceb803bb4cc396dc241dfb2dc
enricojunior <zethsuboaragonchemistry@gmail.com>
2024-06-26 11:04:44 +0800

    Setting Initial Auth

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 
-export const useSignup = () => {
+export const useLogin = () => {
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(null)
     const { dispatch } = useAuthContext()
 
-    const signup = async (email, password) => {
+    const login = async (email, password) => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/api/user/signup', {
+        const response = await fetch('/api/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
@@ -24,14 +24,11 @@ export const useSignup = () => {
         }
 
         if(response.ok){
-            // Save the User to Local Storage
             localStorage.setItem('user', JSON.stringify(json))
-
-            // Update the AuthContext
             dispatch({type: 'LOGIN', payload: json})
             setIsLoading(false)
         }
     }
 
-    return { signup, isLoading, error }
+    return { login, isLoading, error }
 }
